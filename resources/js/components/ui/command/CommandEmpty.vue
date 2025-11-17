91b9a5f63ebae4cb40ea7454abdb927e3ae1096e
Arifin Nor <arifinnor03@gmail.com>
2025-11-17 19:32:50 +0700

    refactor: reinstall command and popover component

--- resources/js/components/ui/command/CommandEmpty.vue
+++ resources/js/components/ui/command/CommandEmpty.vue
@@ -1,24 +1,23 @@
 <script setup lang="ts">
-import { CommandEmpty as CommandEmptyPrimitive, type CommandEmptyProps, useForwardProps } from 'reka-ui'
-import { cn } from '@/lib/utils'
-import { computed, type HTMLAttributes } from 'vue'
+import type { PrimitiveProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { reactiveOmit } from "@vueuse/core"
+import { Primitive } from "reka-ui"
+import { computed } from "vue"
+import { cn } from "@/lib/utils"
+import { useCommand } from "."
 
-const props = defineProps<CommandEmptyProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<PrimitiveProps & { class?: HTMLAttributes["class"] }>()
 
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
-  return delegated
-})
+const delegatedProps = reactiveOmit(props, "class")
 
-const forwarded = useForwardProps(delegatedProps)
+const { filterState } = useCommand()
+const isRender = computed(() => !!filterState.search && filterState.filtered.count === 0,
+)
 </script>
 
 <template>
-  <CommandEmptyPrimitive
-    v-bind="forwarded"
-    :class="cn('py-6 text-center text-sm', props.class)"
-  >
+  <Primitive v-if="isRender" v-bind="delegatedProps" :class="cn('py-6 text-center text-sm', props.class)">
     <slot />
-  </CommandEmptyPrimitive>
+  </Primitive>
 </template>
-
--- resources/js/components/ui/command/CommandGroup.vue
+++ resources/js/components/ui/command/CommandGroup.vue
@@ -1,24 +1,44 @@
 <script setup lang="ts">
-import { CommandGroup as CommandGroupPrimitive, type CommandGroupProps, useForwardProps } from 'reka-ui'
-import { cn } from '@/lib/utils'
-import { computed, type HTMLAttributes } from 'vue'
+import type { ListboxGroupProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { reactiveOmit } from "@vueuse/core"
+import { ListboxGroup, ListboxGroupLabel, useId } from "reka-ui"
+import { computed, onMounted, onUnmounted } from "vue"
+import { cn } from "@/lib/utils"
+import { provideCommandGroupContext, useCommand } from "."
 
-const props = defineProps<CommandGroupProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<ListboxGroupProps & {
+  class?: HTMLAttributes["class"]
+  heading?: string
+}>()
 
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
-  return delegated
+const delegatedProps = reactiveOmit(props, "class")
+
+const { allGroups, filterState } = useCommand()
+const id = useId()
+
+const isRender = computed(() => !filterState.search ? true : filterState.filtered.groups.has(id))
+
+provideCommandGroupContext({ id })
+onMounted(() => {
+  if (!allGroups.value.has(id))
+    allGroups.value.set(id, new Set())
 })
-
-const forwarded = useForwardProps(delegatedProps)
+onUnmounted(() => {
+  allGroups.value.delete(id)
+})
 </script>
 
 <template>
-  <CommandGroupPrimitive
-    v-bind="forwarded"
+  <ListboxGroup
+    v-bind="delegatedProps"
+    :id="id"
     :class="cn('overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground', props.class)"
+    :hidden="isRender ? undefined : true"
   >
+    <ListboxGroupLabel v-if="heading" class="px-2 py-1.5 text-xs font-medium text-muted-foreground">
+      {{ heading }}
+    </ListboxGroupLabel>
     <slot />
-  </CommandGroupPrimitive>
+  </ListboxGroup>
 </template>
-
